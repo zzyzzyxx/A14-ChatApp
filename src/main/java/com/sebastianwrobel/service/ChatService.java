9830b01fc60b5110c08cbfc120a40a12c015681d
zzyzzyxx <molken24@gmail.com>
2023-03-14 16:50:47 +0000

    service package update

--- src/main/java/com/sebastianwrobel/service/ChatService.java
+++
@@ -1,5 +0,0 @@
-package com.sebastianwrobel.service;
-
-public class ChatService {
-
-}
--- src/main/java/com/sebastianwrobel/web/ChatController.java
+++ src/main/java/com/sebastianwrobel/web/ChatController.java
@@ -1,6 +1,5 @@
 package com.sebastianwrobel.web;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,11 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
 import com.sebastianwrobel.dto.Channel;
-import com.sebastianwrobel.dto.User;
 import com.sebastianwrobel.service.ChannelService;
 import com.sebastianwrobel.service.UserService;
 
